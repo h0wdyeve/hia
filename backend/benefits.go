ad1168f9bdbe9f811ad09e2e072698182c0b88ea
h0wdyeve <rawitsara6094@gmail.com>
2024-09-21 15:10:48 +0700

    Create benefits.go

---
+++ backend/benefits.go
@@ -0,0 +1,21 @@
+package entity
+
+import "gorm.io/gorm"
+
+type Benefits struct {
+	gorm.Model
+	BenefitsName	string
+	FlyingFrom		string
+	GoingTo			string
+	PointRequired	uint
+	Quantity		uint
+	Code			string
+	Trip			string
+	Type			string
+
+	AirlineID *uint
+	Airline Airline `gorm:"foreignKey:AirlineID"`
+
+	AdminID *uint
+	Admin Admin `gorm:"foreignKey:AdminID"`
+}
